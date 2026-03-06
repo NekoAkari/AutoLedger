f8f539b4fa39e35685f231c80988058271f4997f
Ella Long <sla554@sfu.ca>
2026-03-05 21:33:15 -0800

    Initial Commit

---
+++ AutoLedger/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AutoLedger
+//
+//  Created by Neko Akari on 2026-03-05.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
