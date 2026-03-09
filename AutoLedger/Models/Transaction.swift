897a57924b1936222d7a063776cb0789da016e26
Ella Long <sla554@sfu.ca>
2026-03-08 20:39:47 -0700

    feat: add transaction data model using SwiftData

--- AutoLedger/Models/Transaction.swift
+++ AutoLedger/Models/Transaction.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import SwiftData
+
+/// Represents a financial transacton
+@Model
+final class Transaction {
+    
+    // MARK: - Properties
+    
+    var date: Date
+    var amount: Double
+    var type: String
+    var category: String
+    var note: String?
+    
+    // MARK: - Initializer
+    init(
+        date: Date,
+         amount: Double,
+         type: String,
+         category: String,
+         note: String? = nil
+    ) {
+        self.date = date
+        self.amount = amount
+        self.type = type
+        self.category = category
+        self.note = note
+    }
+}
